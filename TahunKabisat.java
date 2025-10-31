1a8b627c74a599c82933e44146341c3ad052993f
rafii <decik733@gmail.com>
2025-10-31 19:11:09 +0700

    no 4

---
+++ TahunKabisat.java
@@ -0,0 +1,18 @@
+
+package PBOpraktikum;
+
+import java.util.Scanner;
+
+public class TahunKabisat {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan tahun: ");
+        int tahun = input.nextInt();
+
+        if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
+            System.out.println(tahun + " adalah tahun kabisat.");
+        } else {
+            System.out.println(tahun + " bukan tahun kabisat.");
+        }
+    }
+}
